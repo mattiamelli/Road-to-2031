226c8684ea6db804a645c41255a76d2fe66e9074
Mattia Melli <mattiamelli@Mattias-MacBook-Pro.local>
2026-08-27 11:16:42 +0200

    First commit

---
+++ 02 - Projects/01_python_basics/hello.py
@@ -0,0 +1,5 @@
+nome = input("Quale è il tuo nome?")
+anni = int(input("Quanti anni hai ad oggi?"))
+print(f"Ciao {nome} nel 2031 avrai:{anni} anni")
+
+
---
+++ 02 - Projects/01_python_basics/mini_test.py
@@ -0,0 +1,8 @@
+def valore_medio (somma_voti, numero_voti):
+    print(f"Il valore medio dei voti è pari alla somma dei tuoi voti diviso il numero di voti, ovvero {somma_voti}/{numero_voti}")
+    return somma_voti7numero_voti
+def main():
+    somma_voti = int(input("Inserisci la somma dei tuoi voti, "))
+    numero_voti = int(input("Inserisci il numero dei tuoi voti, "))
+    print(f"Il valore medio dei voti è pari alla somma dei tuoi voti diviso il numero di voti, ovvero {somma_voti}/{numero_voti}")
+main()
